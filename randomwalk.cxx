d0e450ecdba0481e2a1412f87e45270be288c6a3
theo90 <theo_nikolova@yahoo.com>
2015-11-18 18:09:37 +0100

    Update randomwalk.cxx

--- randomwalk.cxx
+++ randomwalk.cxx
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
-
+#include <cmath>
 using namespace std;
 
 struct colloid{
@@ -11,6 +11,9 @@ struct colloid{
 
 void init(colloid* const c, const int N);
 void print(const colloid* const c, const int N, const string fname);
+void random(int* rx, int* ry, int N);
+void pusher(colloid* const c, const int N, int* rx, int* ry, double step);
+void statistic (colloid *const c,int *rx, int *ry, const int N, double & meanx, double & meany,double & var);
 
 int main(void){
   
@@ -44,6 +47,13 @@ int main(void){
     
     for(int i = 1; i <= Nfiles; i++){
 	for(int j = 0; j < Nsubsteps; j++){
+	    random( rx, ry,N);
+	    pusher( c,  N, rx,  ry, step);
+	    meanx=0;
+	    meany=0;
+	    var=0;
+	    statistic (c, rx, ry, N,  meanx,  meany, var);
+	    
 	    // call to function which randomly sets up rx and ry
 	    // call to function which pushes all colloids according to rx and ry
 	    // call to function which evaluates statistics
@@ -72,4 +82,46 @@ void print(const colloid* const c, const int N, const string fname){
     for(int i = 0; i < N; i++)
 	out << c[i].x << "\t" << c[i].y << endl;
     out.close();
+}
+
+void random(int* rx, int* ry, int N)
+
+{
+  for(int k=0; k<N; k++)
+	  {
+	    rx[k]=int(3*(double (rand())/RAND_MAX))-1;
+	    ry[k]=int(3*(double (rand())/RAND_MAX))-1;
+	  }
+}
+
+void pusher(colloid* const c, const int N, int* rx, int* ry, double step)
+{
+  for(int i=0; i<N; i++)
+  {
+    c[i].x=c[i].x+rx[i]*step;
+    c[i].y=c[i].y+ry[i]*step;
+  }
+}
+
+void statistic (colloid *const c,int *rx, int *ry, const int N, double & meanx, double & meany, double & var)
+{
+  double sum1=0;
+  double sum2=0;
+  double var1=0, var2=0;
+  for(int i=0; i<N; i++)
+  {
+    sum1+=c[i].x;
+    sum2+=c[i].y;
+    
+  }
+  meanx=sum1/N;
+  meany=sum2/N;
+  
+  for(int i=0; i<N; i++)
+  {
+    var1+=pow((c[i].x-meanx),2);
+    var2+=pow((c[i].y-meany),2);
+  }
+  var=var1+var2;
+    
 }
